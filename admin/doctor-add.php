f8cc34c939d2c8d5adef7aeaf7d1cc8c987a96b2
AJ <mahidlawonaljun@gmail.com>
2023-04-05 21:23:01 +0800

    Add logics for inserting  data

--- admin/doctor-add.php
+++ admin/doctor-add.php
@@ -1,43 +1,94 @@
 <?php  include '../inc/header.php';?>
+<?php include '../inc/connection.php';
+
+session_start();
+
+if(isset($_POST['submit'])){
+  $first_name = $_POST['first_name'];
+  $last_name = $_POST['last_name'];
+  $address = $_POST['address'];
+  $email = $_POST['email'];
+  $department = $_POST['department'];
+
+  if(!empty($first_name) && !empty($last_name) && !empty($address) && !empty($email) && !empty($department)){
+    $sql = "INSERT INTO doctors(first_name,last_name,address,email,department) 
+                        VAlUES('$first_name','$last_name','$address','$email','$department')";
+
+    $result = $conn->query($sql);
+
+    if($result){
+      $_SESSION['message'] = '
+        <div class="alert alert-success py-3 alert-dismissible" role="alert">
+          <div>Doctor added successfully</div>
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>';
+      }else{
+        $_SESSION['message'] = '
+          <div class="alert alert-warning py-3 alert-dismissible" role="alert">
+            <div>Failed to add new Doctor</div>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>';
+      }
+
+      header('Location:dashboard.php');
+  }else{
+     $_SESSION['message'] = '
+        <div class="alert alert-warning text-center" role="alert">
+          Please fill out all input fields!
+        </div>';
+  }
+  
+  $conn->close();
+}
+
+?>
 
 <div class="container-fluid">
-  <div class="row mb-5">
+  <div class="row">
     <div class="col p-2 bg-primary">
-       <h1 class="text-white h3 d-flex justify-content-center align-items-center gap-2">
-          <i class="bi bi-universal-access"></i>
-          <p class="p-0 m-0">Hospital</p>
-        </h1>
+      <h1 class="text-white h3 d-flex justify-content-center align-items-center gap-2">
+        <i class="bi bi-universal-access"></i>
+        <p class="p-0 m-0">Hospital</p>
+      </h1>
     </div>
   </div>
 </div>
-<div class="container">
+
+<?php 
+  if(isset($_SESSION['message'])){
+    echo $_SESSION['message'];
+  }
+?>
+
+<div class="container mt-5">
   <div class="row justify-content-center">
     <div class="col-6">
-      <form action="#" method="" class="py-5 px-4 border border-opacity-50 border-secondary rounded shadow-lg">
+      <form action="doctor-add.php" method="POST"
+        class="py-5 px-4 border border-opacity-50 border-secondary rounded shadow-lg">
         <div class="row justify-content-between align-items-center">
           <div class="col form-floating mb-3">
-            <input type="text" class="form-control" id="firstname" placeholder="Enter your firstname">
+            <input type="text" class="form-control" id="firstname" name="first_name" placeholder="Enter your firstname">
             <label for="firstname" class="ps-4 fw-light">Firstname</label>
           </div>
           <div class="col form-floating mb-3">
-            <input type="text" class="form-control" id="lastname" placeholder="Enter your lastname">
+            <input type="text" class="form-control" id="lastname" name="last_name" placeholder="Enter your lastname">
             <label for="lastname" class="ps-4 fw-light">Lastname</label>
           </div>
         </div>
         <div class="form-floating mb-3 ">
-          <input type="text" class="form-control " id="address" placeholder="Enter your address">
+          <input type="text" class="form-control " id="address" name="address" placeholder="Enter your address">
           <label for="address" class="fw-light">Address</label>
         </div>
         <div class="form-floating mb-4">
-          <input type="mail" class="form-control" id="email" placeholder="Enter your email">
+          <input type="mail" class="form-control" id="email" name="email" placeholder="Enter your email">
           <label for="email" class="fw-light">Email</label>
         </div>
         <div class="form-floating mb-4">
-          <input type="mail" class="form-control" id="department" placeholder="Enter your department">
+          <input type="mail" class="form-control" id="department" name="department" placeholder="Enter your department">
           <label for="department" class="fw-light">Department</label>
         </div>
         <div class="d-grid">
-          <button class="btn btn-success">Submit</button>
+          <button type="submit" class="btn btn-success" name="submit">Submit</button>
         </div>
       </form>
     </div>
