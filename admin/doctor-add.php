871401b3fb4d37f35427fa87f82fce15ad476a18
AJ <mahidlawonaljun@gmail.com>
2023-04-06 22:12:28 +0800

    Test how to insert image and fect it. Will update later for the design

--- admin/doctor-add.php
+++ admin/doctor-add.php
@@ -10,9 +10,24 @@ if(isset($_POST['submit'])){
   $email = $_POST['email'];
   $department = $_POST['department'];
 
-  if(!empty($first_name) && !empty($last_name) && !empty($address) && !empty($email) && !empty($department)){
-    $sql = "INSERT INTO doctors(first_name,last_name,address,email,department) 
-                        VAlUES('$first_name','$last_name','$address','$email','$department')";
+  $image = $_FILES['image'];
+
+  print_r($image) ;
+  $image_name =  $image['full_path'];
+  echo '<br>';
+  $image_tmp = $image['tmp_name'];
+  echo '<br>';
+  print_r($image_name) ;
+  echo '<br>';
+
+  $folder = '../assets/'.$image_name;
+  print_r($folder) ;
+  move_uploaded_file($image_tmp,$folder);
+
+
+  if(!empty($first_name) && !empty($last_name) && !empty($address) && !empty($email)  && !empty($image)){
+    $sql = "INSERT INTO doctors(first_name,last_name,address,email,department,image) 
+                        VAlUES('$first_name','$last_name','$address','$email','$department','$folder')";
 
     $result = $conn->query($sql);
 
@@ -23,14 +38,15 @@ if(isset($_POST['submit'])){
           <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>';
       }else{
-        $_SESSION['message'] = '
-          <div class="alert alert-warning py-3 alert-dismissible" role="alert">
-            <div>Failed to add new Doctor</div>
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-          </div>';
+        // $_SESSION['message'] = '
+        //   <div class="alert alert-warning py-3 alert-dismissible" role="alert">
+        //     <div>Failed to add new Doctor</div>
+        //     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        //   </div>';
+        die($sql . $conn->error);
       }
 
-      header('Location:dashboard.php');
+      //header('Location:dashboard.php');
   }else{
      $_SESSION['message'] = '
         <div class="alert alert-warning text-center" role="alert">
@@ -41,7 +57,22 @@ if(isset($_POST['submit'])){
   $conn->close();
 }
 
+
+
 ?>
+
+<?php
+    // SQL GET DATA statement
+    $sql = 'SELECT * FROM doctors';
+    $result = mysqli_query($conn, $sql);
+    // Fetch all data in db
+    $rows =  mysqli_fetch_all($result, MYSQLI_ASSOC);
+?>
+
+<?php foreach ($rows as $row) : ?>
+<p><?php echo $row['id']; ?></p>
+<img src="<?php echo $row['image']; ?>"><img>
+<?php endforeach?>
 
 <div class="container-fluid">
   <div class="row">
@@ -63,7 +94,7 @@ if(isset($_POST['submit'])){
 <div class="container mt-5">
   <div class="row justify-content-center">
     <div class="col-6">
-      <form action="doctor-add.php" method="POST"
+      <form action="doctor-add.php" method="POST" enctype="multipart/form-data"
         class="py-5 px-4 border border-opacity-50 border-secondary rounded shadow-lg">
         <div class="row justify-content-between align-items-center">
           <div class="col form-floating mb-3">
@@ -86,6 +117,13 @@ if(isset($_POST['submit'])){
         <div class="form-floating mb-4">
           <input type="mail" class="form-control" id="department" name="department" placeholder="Enter your department">
           <label for="department" class="fw-light">Department</label>
+        </div>
+        <div class="mb-4">
+          <label for="imgs" class="form-label">Choose a picture</label>
+          <input class="form-control" type="file" id="imgs" name="image" placeholder="Choose a picture">
+        </div>
+
+        <div class=" input-group">
         </div>
         <div class="d-grid">
           <button type="submit" class="btn btn-success" name="submit">Submit</button>
