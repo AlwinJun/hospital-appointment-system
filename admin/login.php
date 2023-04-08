fdb73976d3c35d64e81a6ee048505639046835f4
AJ <mahidlawonaljun@gmail.com>
2023-04-08 22:23:34 +0800

    Add code needed for login process and error messages

--- admin/login.php
+++ admin/login.php
@@ -3,20 +3,28 @@
 <div class="container-fluid">
   <div class="row justify-content-center align-items-center vh-100">
     <div class="col-5">
-      <form action="#" method="" class="py-5 px-4 border border-opacity-50 border-secondary rounded shadow-lg">
-          <div class="form-floating mb-3">
-            <input type="text" class="form-control" id="username" placeholder="Enter your username">
-            <label for="username" class="form-label fw-light">Username</label>
-          </div>
+      <form action="process.php" method="POST"
+        class="py-5 px-4 border border-opacity-50 border-secondary rounded shadow-lg">
+        <?php 
+          session_start();
+          if(isset($_SESSION['error'])){
+            echo $_SESSION['error'];
+            unset($_SESSION['error']);
+          }
+        ?>
+        <div class="form-floating mb-3">
+          <input type="text" class="form-control" id="username" name="user" placeholder="Enter your username">
+          <label for="username" class="form-label fw-light">Username</label>
+        </div>
 
-          <div class="form-flotaing mb-4">
-            <input type="text" class="form-control" id="password" placeholder="Enter your password">
-            <label for="password" class="form-label fw-light">Password</label>
-          </div>
+        <div class="form-floating mb-4">
+          <input type="text" class="form-control" id="password" name="pass" placeholder="Enter your password">
+          <label for="password" class="form-label fw-light">Password</label>
+        </div>
 
-          <div class="d-grid">
-                <button class="btn btn-primary">Log-in</button>
-          </div>
+        <div class="d-grid">
+          <button class="btn btn-primary" name="login_admin">Log-in</button>
+        </div>
       </form>
     </div>
   </div>
