78c0ca03bc68c8df755507ca254213af235c7c6e
AJ <mahidlawonaljun@gmail.com>
2023-04-10 08:59:34 +0800

    Update 404

--- 404.php
+++ 404.php
@@ -1,2 +1,11 @@
-<P>404</P>
-<p>Oops!Page not found.</p>
+<?php  include 'inc/header.php';?>
+<div class="container">
+  <div class="d-flex flex-column align-items-center justify-content-center vh-100">
+    <p class="h1 fw-bold">
+      404
+    </p>
+    <p class="lead fw-semibold">Sorry, this page isn't available.</p>
+  </div>
+</div>
+
+<?php include 'inc/footer.php';?>
